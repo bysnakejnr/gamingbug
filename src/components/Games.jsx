15de550ca3a693c44ca4a90b458a0a9f2002262d
bysnakejnr <barisberber.ca@gmail.com>
2024-06-17 20:38:39 -0400

    Mobile bugs fixed, animations were fixed.

--- src/components/Games.jsx
+++ src/components/Games.jsx
@@ -13,9 +13,9 @@ function Games() {
 
                         <div className="card-body bg-game-image1 bg-center bg-cover flex flex-col text-white overflow-auto place-content-center">
 
-                            <h2 className="heavy-font text-lg sm:text-4xl">Ship and Crew Management</h2>
+                            <h2 className="heavy-font text-lg lg:text-4xl md:text-4xl sm:text-2xl xl:text-4xl">Ship and Crew Management</h2>
                             <br/><br/>
-                            <p className="text-s max-w-96 sm:text-xl">
+                            <p className="text-xs max-w-96 lg:text-lg md:text-lg sm:text-sm xl:text-lg">
                                 Command your own pirate ship and crew,
                                 making decisions that would make even the
                                 most seasoned buccaneer proud. Upgrade and
@@ -27,9 +27,9 @@ function Games() {
                     <li className="card mt-12" id="card2">
                         <div
                             className="card-body bg-game-image2 bg-center bg-cover flex flex-col text-white overflow-auto place-content-center">
-                            <h2 className="heavy-font text-lg sm:text-4xl">Treasure Hunting</h2>
+                            <h2 className="heavy-font text-lg lg:text-4xl md:text-4xl sm:text-2xl xl:text-4xl">Treasure Hunting</h2>
                             <br/><br/>
-                            <p className="text-s max-w-96 sm:text-xl">
+                            <p className="text-xs max-w-96 lg:text-lg md:text-lg sm:text-sm xl:text-lg">
                                 Embark on a treasure hunt of epic proportions,
                                 inspired by the legendary tales of pirate lore.
                                 Seek hidden treasures and follow cryptic maps to uncover
@@ -40,9 +40,9 @@ function Games() {
                 <li className="card" id="card3">
                     <div
                         className="card-body bg-game-image3 bg-center bg-cover flex flex-col text-white overflow-auto place-content-center">
-                        <h2 className="heavy-font text-lg sm:text-4xl">Co-Op Gameplay</h2>
+                        <h2 className="heavy-font text-lg lg:text-4xl md:text-4xl sm:text-2xl xl:text-4xl">Co-Op Gameplay</h2>
                         <br/><br/>
-                        <p className="text-s max-w-96 sm:text-xl">
+                        <p className="text-xs max-w-96 lg:text-lg md:text-lg sm:text-sm xl:text-lg">
                             Abide by the pirate's code and form alliances with your shipmates.
                             Team up to face perilous challenges, engage in epic naval battles,
                             and conquer the high seas together. Or, let treachery be your guide as
@@ -51,12 +51,12 @@ function Games() {
                 </li>
                 <li className="card pb-5" id="card4">
                     <div className="card-body flex flex-col text-white overflow-auto place-content-center">
-                        <h2 className="heavy-font text-lg sm:text-4xl">And More!</h2>
-                        <br/><br/>
-                        <p className="text-s max-w-96 sm:text-xl">
+                        <h2 className="heavy-font text-lg lg:text-4xl md:text-4xl sm:text-2xl xl:text-4xl">And More!</h2>
+                        <br/>
+                        <p className="text-xs max-w-96 sm:text-xl">
                             Go to one of our social media channels to learn more.</p>
                         <img
-                            src={`${photos}/gameimage4.png`} className="w-44 mt-12"/>
+                            src={`${photos}/gameimage4.png`} className="w-24 sm:w-44 md:w-44 lg:w-44 xl:w-44 mt-12"/>
                         <div className="flex justify-center mt-8 space-x-6">
                             <a href="https://www.linkedin.com/company/gamingbug/?viewAsMember=true "
                                className="icon-linkedin text-black">
